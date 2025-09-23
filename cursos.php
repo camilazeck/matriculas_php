78648dffb1109223840ace69f631944d13094c21
Camila Scheffel Zeck <ca.scheffel.zeck@gmail.com>
2025-09-22 22:21:14 -0300

    aula - 22.09

--- cursos.php
+++ cursos.php
@@ -16,6 +16,7 @@ include_once("conexaodb.php");
       <tr>
         <th>ID do Curso </th>
         <th>Nome do Curso</th>
+        <th colspan="2">Ações</th>
       </tr>
       <?php
       $stmt = $pdo->prepare("SELECT * FROM cursointeresse");
@@ -28,17 +29,37 @@ include_once("conexaodb.php");
         echo "<tr>";
         echo "<td>" . $linha['idCursoInteresse'] . "</td>";
         echo "<td>" . $linha['Nome'] . "</td>";
+        echo "<td><a href='curso_delete.php?id_curso=" . $linha['idCursoInteresse'] . "'
+                    onclick='return confirm(\"Tem certeza que deseja excluir o curso selecionado?\")'>Delete</a></td>";
+        echo"<td><a href='cursos.php?id_curso=" . $linha['idCursoInteresse'] . "'>Editar</a></td>";
         echo "</tr>";
       }
     ?>
   </table>
 
+  <?php
+  $id_curso = isset($_GET["id_curso"]) ? $_GET["id_curso"] : '';
+  //exibe o formulário de adicionar novo curso apenas se não estamos editando um curso existente,
+  //ou seja, $id_curso está vazio.
+  if(empty($id_curso)) {
+  ?>
     <h2>Cadastrar novo curso</h2>
-    <form action="curso_add.php" method="get">
-        <label for="nome_curso">Nome do curso:</label>
-        <input type="text" id="nome_curso" name="nome_curso">
-        <br>
-        <input type="submit">
+    <form action="curso_add.php"method="get">
+      <label for="nome_curso">Nome do curso:</label>
+      <input type="text" id="nome_curso" name="nome_curso">
+      <br>
+      <input type="submit">
     </form>
+  <?php } else { ?>
+    <h2>Editar curso existente</h2>
+    <form action="curso_update.php" method="get">
+      <label for="nome_curso">Nome do curso:</label>
+      <input type="text" id="nome_curso" name="nome_curso">
+      <input type="hidden" name="id_curso" value="<?php echo $id_curso; ?>">
+      <br>
+      <input type="submit">
+    </form>
+  </form>
+  <?php } ?>
   </body>
 </html>
